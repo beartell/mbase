3366336574fa22ef888dfff17d2a54e9126d3e07
Emreerdog <emreerdog@hotmail.com>
2023-12-08 02:40:43 +0300

    Started implementing io

---
+++ include/mbase/io_base.h
@@ -0,0 +1,69 @@
+#ifndef MBASE_IOBASE_H
+#define MBASE_IOBASE_H
+
+#include <mbase/common.h>
+#include <mbase/char_stream.h>
+
+MBASE_STD_BEGIN
+
+// IO_BASE IS ABSTRACT CLASS
+struct io_context {
+	PTRGENERIC raw_handle;
+	PTRGENERIC context_body;
+};
+
+class io_base {
+public:
+	using size_type = SIZE_T;
+
+	virtual size_type write_data(IBYTEBUFFER in_src) = 0;
+	virtual size_type write_data(IBYTEBUFFER in_src, size_type in_length) = 0;
+	virtual size_type write_data(const mbase::string& in_src) = 0;
+	virtual size_type write_data(const char_stream& in_src) = 0;
+
+	virtual size_type read_data(IBYTEBUFFER in_src) = 0;
+	virtual size_type read_data(IBYTEBUFFER in_src, size_type in_length) = 0;
+	virtual size_type read_data(const mbase::string& in_src) = 0;
+	virtual size_type read_data(const char_stream& in_src) = 0;
+
+	// ASSOCIATE INPUT CHARACTER STREAM
+	GENERIC associate_is(char_stream& in_stream) noexcept {
+		istream = in_stream;
+	}
+
+	// ASSOCIATE OUTPUT CHARACTER STREAM
+	GENERIC associate_os(char_stream& in_stream) noexcept {
+		ostream = in_stream;
+	}
+
+	size_type sync_is() noexcept {
+		return write_data(istream);
+	}
+
+	size_type sync_os() noexcept {
+		return read_data(ostream);
+	}
+
+	USED_RETURN io_context* get_raw_context() noexcept {
+		return &rawContext;
+	}
+
+protected:
+	GENERIC _set_raw_context(PTRGENERIC raw_handle) noexcept {
+		rawContext.raw_handle = raw_handle;
+		rawContext.context_body = nullptr;
+	}
+
+	GENERIC _set_raw_context(PTRGENERIC raw_handle, PTRGENERIC context_body) noexcept {
+		rawContext.raw_handle = raw_handle;
+		rawContext.context_body = context_body;
+	}
+
+	io_context rawContext;
+	char_stream& istream;
+	char_stream& ostream;
+};
+
+MBASE_STD_END
+
+#endif // !MBASE_IOBASE_H
