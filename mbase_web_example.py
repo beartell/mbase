109fba618a03736cbdf65bab22df4a48b24f0560
Emreerdog <emreerdog@hotmail.com>
2024-09-24 20:26:18 +0300

    F1: CSS Is almost finished. F2: Chat history implemented

--- mbase_web_example.py
+++ mbase_web_example.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask_socketio import SocketIO
 from maip_client import MaipClient
 from maip_operation_object import MaipOperationResult
 import uuid
 import datetime
 import sqlite3
+import json
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+
+# CLIENT CONTEXT MAP
+clientContextMap = {}
 
 # ERRORS LIST:
 operationSuccessful = {"message" : "Success", "code" : 0}
@@ -23,6 +29,9 @@ errMaipPortFormatError = {"message" : "Invalid port format", "code" : 9}
 errModelQueryError = {"message" : "Models couldn't be queried, make sure to check your MAIP host is setup correctly", "code" : 10}
 errMaipMissingParams = {"message" : "MAIP parameters are missing", "code" : 10}
 errContextNotFound = {"message" : "Context not found", "code" : 11}
+errContextInitializeFail = {"message" : "Failed to initialize context on MAIP host.", "code" : 12}
+errUnknownRole = {"message" : "Unknown message role", "code" : 13}
+errMissingMessage = {"message" : "Missing message", "code" : 14}
 
 # STATUS LIST
 serverStatusRunning = 0
@@ -53,7 +62,7 @@ chatHistoryTableCreationString = """
     CREATE TABLE IF NOT EXISTS chat_history(
         chat_id TEXT,
         user_id TEXT,
-        msg_role INTEGER,
+        msg_role TEXT,
         msg_content TEXT,
         msg_index INTEGER,
         FOREIGN KEY(chat_id) REFERENCES contexts(ctx_id)
@@ -92,7 +101,7 @@ def chat_creation_request():
     context_length = request.args.get("ctx_length")
     context_remember = request.args.get("ctx_remember")
     system_prompt = request.args.get("system_prompt")
-
+  
     if maip_host is None or maip_port is None or selected_model is None or context_length is None or context_remember is None or system_prompt is None:
         return {"status": errInvalidCreateParams}
 
@@ -133,6 +142,58 @@ def chat_creation_request():
         dbConnection.commit()
         return {"status" : operationSuccessful, "chat_id" : newChatId}
     return {"status" : errDbConnectionFail}
+
+@app.route("/chat/append")
+def chat_append():
+    mbase_id = request.cookies.get("mb-id")
+
+    if mbase_id is None:
+        return {"status" : errUnauthorizedClient}
+    with sqlite3.connect("mbase_web_program.db") as dbConnection:
+        resultCursor = dbConnection.execute("SELECT * FROM users WHERE mbase_id=?", (mbase_id,))
+        if resultCursor.fetchone() is None:
+            return {"status" : errUnregisteredClient}
+    
+    chat_id = request.args.get("chat_id")
+    message_role = request.args.get("msg_role")
+    msg_content = request.args.get("message")
+
+    if message_role != "Assistant" and message_role != "User":
+        return {"status" : errUnknownRole}
+
+    if len(msg_content) == 0:
+        return {"status" : errMissingMessage}
+    
+    with sqlite3.connect("mbase_web_program.db") as dbConnection:
+        resultCursor = dbConnection.execute("INSERT INTO chat_history(chat_id, user_id, msg_role, msg_content, msg_index) VALUES(?, ?, ?, ?, (SELECT IFNULL(MAX(msg_index), 0) + 1 FROM chat_history WHERE chat_id=?))", (chat_id, mbase_id, message_role, msg_content, chat_id))
+    return {"status" : operationSuccessful}
+
+@app.route("/chat/get_chat_history/<string:chat_id>")
+def get_chat_history(chat_id):
+    mbase_id = request.cookies.get("mb-id")
+
+    if mbase_id is None:
+        return {"status" : errUnauthorizedClient}
+    with sqlite3.connect("mbase_web_program.db") as dbConnection:
+        resultCursor = dbConnection.execute("SELECT * FROM users WHERE mbase_id=?", (mbase_id,))
+        if resultCursor.fetchone() is None:
+            return {"status" : errUnregisteredClient}
+        resultCursor = dbConnection.execute("SELECT msg_role, msg_content, msg_index FROM chat_history WHERE user_id=? AND chat_id=? ORDER BY msg_index ASC", (mbase_id, chat_id))
+        fetchResult = resultCursor.fetchall()
+        messageList = []
+        for i in fetchResult:
+            msgRole = i[0]
+            msgContent = i[1]
+            msgIndex = i[2]
+            historyInfo = {
+                "msg_role" : msgRole,
+                "msg_content" : msgContent,
+                "msg_index" : msgIndex
+            }
+            messageList.append(historyInfo)
+        return {"status" : operationSuccessful, "message_history" : messageList}
+    return {"status" : errDbConnectionFail}
+
 
 @app.route("/chat/get_chats")
 def get_chats():
@@ -273,7 +334,87 @@ def server_status():
             return {"status" : operationSuccessful, "server_status" : serverStatusRunning}
     
     # model is not supported
-    return {"status" : operationSuccessful, "server_status" : serverStatusModelNotSupported}
+    return {"status" : operationSuccessful, "server_status" : serverStatusModelNotSupported, "models" : operationResult.get_result_data()}
 
+@app.route("/maip/initialize_context")
+def initialize_context():
+    mbase_id = request.cookies.get("mb-id")
+
+    if mbase_id is None:
+        return {"status" : errUnauthorizedClient}
+    with sqlite3.connect("mbase_web_program.db") as dbConnection:
+        resultCursor = dbConnection.execute("SELECT * FROM users WHERE mbase_id=?", (mbase_id,))
+        if resultCursor.fetchone() is None:
+            return {"status" : errUnregisteredClient}
+     
+    maip_host = request.args.get("maip_host")
+    maip_port = request.args.get("maip_port")
+    selected_model = request.args.get("selected_model")
+    context_length = request.args.get("ctx_length")
+    context_remember = request.args.get("ctx_remember")
+    request.get_json(force=True)
+    if maip_host is None or maip_port is None or selected_model is None or context_length is None or context_remember is None:
+        return {"status": errInvalidCreateParams}
+
+    try:
+        maip_port = int(maip_port)
+        context_length = int(context_length)
+        pass
+    except ValueError:
+        return {"status": errIntegerConstrainFail}
+    
+    if context_length < 32:
+        return {"status" : errContextLengthFail}
+
+    if clientContextMap.get(mbase_id) is None:
+        pass
+    else:
+        # delete the context
+        pass
+    
+    maipClient : MaipClient = MaipClient(maip_host, maip_port)
+    if maipClient.is_connection_active() is False:
+        return {"status" : errMaipConnectionFail} 
+    
+    myResult : MaipOperationResult = maipClient.create_context(selected_model, context_length)
+    if myResult.get_result_code() != 2016 or myResult.get_result_code() != 2000:
+        return {"status" : errContextInitializeFail}
+    
+    clientContextMap.setdefault(mbase_id, {"context_handle" : myResult.get_result_data(), "msg_ids" : []})
+    return {"status" : operationSuccessful}
+
+@app.route("/maip/create_message_id")
+def create_message_id():
+    mbase_id = request.cookies.get("mb-id")
+    if mbase_id is None:
+        return {"status" : errUnauthorizedClient}
+    with sqlite3.connect("mbase_web_program.db") as dbConnection:
+        resultCursor = dbConnection.execute("SELECT * FROM users WHERE mbase_id=?", (mbase_id,))
+        if resultCursor.fetchone() is None:
+            return {"status" : errUnregisteredClient}
+
+
+@socketio.on('init_context')
+def initialize_context(data):
+    # maip_host
+    # maip_port
+    # context length
+    # selected model
+    
+    initContextDict : dict = json.loads(data)
+    
+    clientContextMap.setdefault(initContextDict.get("mbase-id"), initContextDict)
+
+    # if it is present, delete the active context
+
+@socketio.on('execute_input')
+def execute_input(data):
+    pass
+
+@socketio.on('get_next')
+def get_next(data):
+    pass
+
+socketio.run(app=app)
 #     myClient = MaipClient()
 #     return {"maiphost": uuid.uuid4(), "port" : request.args.get("maip_port")}
